e02110786eae15c0908b4b40ef36e03e19af081d
Tamas Hajdu <info@ezmapdesign.com>
2025-03-02 13:04:44 +0100

    Minor changes to deleteMemory method

--- src/controllers/memories/memoriesController.js
+++ src/controllers/memories/memoriesController.js
@@ -4,6 +4,9 @@ require('dotenv').config();
 // Load model(s)
 const MemoryModel = require('../../models/MemoryModel')
 
+// Load utils
+const utils = require('../../util/utilities')
+
 const saveMemory = async(memoryObj) => {
   try{
     const memoryInsertion = await MemoryModel.createMemory(memoryObj)
@@ -34,6 +37,19 @@ const getAllMemories = async (req, res, next) => {
   }
 }
 
+const getMemoriesForUser = async (req, res, next) => {
+  try {
+    const memoryList = await MemoryModel.getMemoryForUser(req.params.user_id)
+    res.status(200).json(memoryList)
+  }
+  catch(error) {
+    res.status(400).json({
+      status: 'error',
+      message: error
+    })
+  }
+}
+
 const getMemoryFiguresByCountry = async (req, res, next) => {
   try {
     const memoryFiguresByCountry = await MemoryModel.getMemoryCountByCountry()
@@ -44,9 +60,33 @@ const getMemoryFiguresByCountry = async (req, res, next) => {
   }
 }
 
+const deleteMemory = async (req, res, next) => {
+  try{
+    const memoryDeletion = await MemoryModel.deleteMemory(req.params.memory_id)   
+    const s3Deleted = await utils.deleteS3ImageByURL(memoryDeletion.data.s3_url)
+    
+    if(memoryDeletion.status === "success" && s3Deleted.success === true) {
+      res.status(200).json(memoryDeletion)
+    }
+    else
+    {
+      res.status(400).json({
+        status: 'error',
+        message: 'Internal Server Error'
+      })      
+    }
+  }
+  catch(error) {
+    console.log(error)
+    res.status(400).json(error)
+  }
+}
+
 module.exports = {
   saveMemory: saveMemory,
   getLikesForMemory: getLikesForMemory,
   getAllMemories: getAllMemories,
-  getMemoryFiguresByCountry: getMemoryFiguresByCountry
+  getMemoryFiguresByCountry: getMemoryFiguresByCountry,
+  getMemoriesForUser: getMemoriesForUser,
+  deleteMemory: deleteMemory
 }
