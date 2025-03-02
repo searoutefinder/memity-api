a7d3ed4257da52cc940056efbe906ce49ce5e0e1
Tamas Hajdu <info@ezmapdesign.com>
2025-03-02 13:03:39 +0100

    Added singular routes: memory and user to make delete request paths cleaner

--- src/routes/routes-v1.js
+++ src/routes/routes-v1.js
@@ -4,15 +4,19 @@ const router = express.Router();
 
 // Invoke logic for endpoints
 const endpts = {
+  user: require('./v1/user'),
   users: require('./v1/users'),
   memories: require('./v1/memories'),
+  memory: require('./v1/memory'),
   likes: require('./v1/likes'),
   maplabels: require('./v1/maplabels'),
   me: require('./v1/me')
 }
 
+router.use('/user', endpts.user)
 router.use('/users', endpts.users)
 router.use('/memories', endpts.memories)
+router.use('/memory', endpts.memory)
 router.use('/likes', endpts.likes)
 router.use('/maplabels', endpts.maplabels)
 router.use('/me', endpts.me)
