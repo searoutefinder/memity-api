b70244f6191b641e547dfdfb06531455e7b2c67f
Tamas Hajdu <info@ezmapdesign.com>
2025-03-02 13:08:07 +0100

    Refactored canUserSave from the controller to the model

--- src/controllers/users/usersController.js
+++ src/controllers/users/usersController.js
@@ -2,18 +2,77 @@
 require('dotenv').config();
 
 // Load services
+const db = require('../../config/dbService');
 const UsersModel = require('../../models/UsersModel')
+const MemoryModel = require('../../models/MemoryModel')
+
+// Load utils
+const utils = require('../../util/utilities')
+
+const canUserSave = async (user_id) => {
+  try{
+    const canUserSave = await UsersModel.canUserSave(user_id)  
+    return canUserSave
+    }
+    catch(error) {
+      console.log(error)
+      return false  
+  }
+}
 
 const deleteUser = async (req, res, next) => {  
+
   try {
-    const userDeleted = await UsersModel.deleteUser(req.params.user_id)
-    if(userDeleted.status === "success") {
-      res.status(200).json(userDeleted)
-    } 
+
+    // Return if the admin tries to delete himself
+    if(req.user.userId === req.params.user_id) {
+      res.status(400).json({
+        status: "error",
+        message: "This operation is not possible."
+      })
+
+      return
+    }
+
+    const memoryDetails = await MemoryModel.getMemoryForUser(req.params.user_id)
+    
+    // Delete only the user record in case it doesn't have memories associated with it
+    if(memoryDetails.data.length === 0) {
+      const userDeleted = await UsersModel.deleteUser(req.params.user_id)
+      if(userDeleted.status === "success") {
+        res.status(200).json(userDeleted)
+      }
+      else
+      {
+        res.status(400).json({
+          status: 'error',
+          message: 'Internal Server Error'
+        })
+      }      
+    }
+    else
+    {
+      // If the user has memories, also clear them from S3
+      const s3Deleted = await utils.deleteS3ImageByURL(memoryDetails.data[0].s3_url)        
+      const userDeleted = await UsersModel.deleteUser(req.params.user_id)
+    
+      if(userDeleted.status === "success" && s3Deleted.success === true) {
+        res.status(200).json(userDeleted)
+      }
+      else
+      {
+        res.status(400).json({
+          status: 'error',
+          message: 'Internal Server Error'
+        })
+      }       
+    }
+
   }
   catch(error) {
     res.status(400).json(error)
   }
+
 }
 
 const listUsers = async (req, res, next) => {
@@ -28,7 +87,25 @@ const listUsers = async (req, res, next) => {
   }
 }
 
+const listUserSuggestions = async (req, res, next) => {
+  try {
+    if(typeof req.query.q === 'undefined' || req.query.q === '') {
+      res.status(400).json({status: 'error', message: 'You must supply a search term!', data: []})
+      return
+    }
+    const suggestions = await UsersModel.getUserSuggestions(req.query.q)
+    if(suggestions.status === "success") {
+      res.status(200).json(suggestions)
+    }    
+  }
+  catch(error) {
+    res.status(400).json(error)
+  }
+}
+
 module.exports = {
   listUsers: listUsers,
-  deleteUser: deleteUser
+  deleteUser: deleteUser,
+  canUserSave: canUserSave,
+  listUserSuggestions: listUserSuggestions
 }
--- src/models/usersModel.js
+++ src/models/usersModel.js
@@ -2,7 +2,7 @@
 require('dotenv').config();
 
 // Load DB service
-const db = require('../config/dbService');
+const db = require('../services/dbService');
 
 const UsersModel = {
   getUsers: async () => {
@@ -22,6 +22,23 @@ const UsersModel = {
       }
     }
   },
+  getUserSuggestions: async (term) => {
+    try {
+        const result = await db.query(`SELECT id, email FROM ${process.env.DB_TABLE_USERS} WHERE email ILIKE $1 LIMIT 5`, [`%${term}%`])
+        return {
+          status: 'success',
+          message: 'Suggestions were retrieved successfully',
+          data: result
+        }       
+      }
+      catch(error) {
+        return {
+          status: 'error',
+          message: error,
+          data: []
+        }
+      }    
+  },
   deleteUser: async (user_id) => {
     try {
       const result = await db.query(`DELETE FROM ${process.env.DB_TABLE_USERS} WHERE id = $1`, [user_id])
@@ -37,6 +54,15 @@ const UsersModel = {
         message: error,
         data: []
       }
+    }
+  },
+  canUserSave: async (user_id) => {
+    try {
+      const result = await db.query(`SELECT COUNT(*) AS img_cnt FROM ${process.env.DB_TABLE_MEMORIES} WHERE user_id = $1`, [user_id])
+      return result[0].img_cnt > 0 ? false : true
+    }
+    catch(error) {
+      return false
     }
   }
 }
